4f72486d71e5225f72bc0bb52c25b1eaeec24ecc
tanvizuluk <tanvi.zuluk@gmail.com>
2024-04-25 14:56:22 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
+// import Services from "@/components/main/Services"
+import Clients from "@/components/main/Clients";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,8 +12,10 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        {/* <Services /> */}
         <Encryption />
         <Projects />
+        <Clients />
       </div>
     </main>
   );
---
+++ components/main/Services.tsx
@@ -0,0 +1,38 @@
+// import React from "react";
+// import ProjectCard from "../sub/ProjectCard";
+
+// const Services = () => {
+//   return (
+//     <div
+//       className="flex flex-col items-center justify-center py-20"
+//       id="projects"
+//     >
+//       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+//         Our Services
+//       </h1>
+//       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+//         <ProjectCard
+//           src="/NextWebsite.png"
+//           title="Web Development"
+//           description="As a premier website application development company, our certified developers boast unparalleled expertise in harnessing the latest web technologies to deliver tailored web solutions."
+//         />
+//         <ProjectCard
+//           src="/CardImage.png"
+//           title="Mobile App Development"
+//           description="With a proven track record, we specialize in delivering high-impact, result-driven, and engaging mobile application development services tailored for enterprises, small businesses, and startups, whether it's B2B or B2E apps."
+//         />
+//         <ProjectCard
+//           src="/SpaceWebsite.png"
+//           title="UI/UX Design"
+//           description="UI/UX design is crucial for web and mobile apps' success. SuShiv, a leading design agency, specializes in turning ideas into meaningful interactions, from concept to execution."
+//         />
+//         <ProjectCard
+//           src="/SpaceWebsite.png"
+//           title="AI/ML Development"
+//           description="Looking to enhance or create a business app with AI capabilities? Our expert AI-ML engineers can help. We're skilled in TensorFlow, Apache SystemML, Caffe, Apache Mahout, OpenNN, Torch, Mycroft AI, and more."
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
